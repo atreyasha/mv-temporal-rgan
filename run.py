49df60fb42c028136235e34ce873695f0244f778
Atreya Shankar <shankar.atreya@gmail.com>
2019-09-29 01:31:34 +0200

    *shankar* import bugfix

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from RGAN import RGAN
+import glob
 import numpy as np
 import matplotlib.image as mpimg
+from RGAN import RGAN
 
 # compile RGAN and load images
-test = RGAN()
 train_images = [mpimg.imread(file) for file in glob.glob("./data/faces/*")]
 train_images = np.asarray(train_images,dtype="float32")
 train_images /= 255
+# run model and check outout
+test = RGAN()
 test.train(train_images,"test")
