6eb2492488b0d43df59852c4c41d57e8a37e3b32
Atreya Shankar <shankar.atreya@gmail.com>
2019-09-29 02:11:42 +0200

    *shankar* path bugfix

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import matplotlib.image as mpimg
 from RGAN import RGAN
 
 # compile RGAN and load images
-train_images = [mpimg.imread(file) for file in glob.glob("./data/faces/*")]
+train_images = [mpimg.imread(file) for file in glob.glob("./data/lfwcrop_grey/faces/*")]
 train_images = np.asarray(train_images,dtype="float32")
 train_images /= 255
 # run model and check outout
