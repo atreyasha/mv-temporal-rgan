0445ea952874b9c4581d71f70371d6cbfe7cdd07
Atreya Shankar <shankar.atreya@gmail.com>
2019-09-29 02:30:29 +0200

    *shankar* tqdm update

--- run.py
+++ run.py
@@ -5,10 +5,11 @@ import glob
 from tqdm import tqdm
 import numpy as np
 import matplotlib.image as mpimg
+from numpy import resize
 from RGAN import RGAN
 
 # compile RGAN and load images
-train_images = [mpimg.imread(file) for file in tqdm(glob.glob("./data/lfwcrop_grey/faces/*")[:2000])]
+train_images = [resize(mpimg.imread(file),(28,28)) for file in tqdm(glob.glob("./data/lfwcrop_grey/faces/*")[:2000])]
 train_images = np.asarray(train_images,dtype="float32")
 train_images /= 255
 # run model and check outout
