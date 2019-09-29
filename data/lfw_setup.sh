98cb31285bca65470b879410a436605091421f20
Atreya Shankar <shankar.atreya@gmail.com>
2019-09-29 02:22:24 +0200

    *shankar* tqdm update

--- data/lfw_setup.sh
+++ data/lfw_setup.sh
@@ -2,4 +2,4 @@
 set -e
 wget http://conradsanderson.id.au/lfwcrop/lfwcrop_grey.zip
 unzip lfwcrop_grey.zip
-ln -s lfwcrop_grey/faces .
+# ln -s lfwcrop_grey/faces .
--- run.py
+++ run.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import glob
+from tqdm import tqdm
 import numpy as np
 import matplotlib.image as mpimg
 from RGAN import RGAN
 
 # compile RGAN and load images
-train_images = [mpimg.imread(file) for file in glob.glob("./data/lfwcrop_grey/faces/*")]
+train_images = [mpimg.imread(file) for file in tqdm(glob.glob("./data/lfwcrop_grey/faces/*"))]
 train_images = np.asarray(train_images,dtype="float32")
 train_images /= 255
 # run model and check outout
