5543e8162a9e5eba6d8c13ae397b6cd34a21cc27
Atreya Shankar <shankar.atreya@gmail.com>
2019-09-30 14:40:54 +0200

    *shankar* next version of rgan

--- data.py
+++ data.py
@@ -2,6 +2,6 @@ import h5py
 from PIL import Image
 
 fileName = 'data.h5'
-numOfSamples = 10000
+numOfSamples = 13233
 with h5py.File(fileName, "w") as out:
-  out.create_dataset("X_train",(numOfSamples,256,256,3),dtype='u1')
+    out.create_dataset("X",(numOfSamples,[:100] ,256,3),dtype='u1')
