68b70ae22d592b54b29f6578c69c9c5c2cd90ff3
Atreya Shankar <shankar.atreya@gmail.com>
2019-10-02 19:50:40 +0200

    *shankar* minor GPU bugfix

--- obj/RGAN.py
+++ obj/RGAN.py
@@ -23,8 +23,7 @@ class RGAN():
     def __init__(self,latent_dim=28,im_dim=28,epochs=100,batch_size=128,learning_rate=0.0001,
                  g_factor=0.7,droprate=0.2):
         # define and store local variables
-        # TODO: fix keras vs. tensorflow bug later on colab
-        # clear_session()
+        clear_session()
         self.epochs = epochs
         self.batch_size = batch_size
         self.learning_rate = learning_rate
