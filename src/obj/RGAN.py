7e86750e35a0037e256b25fc56b2c591137374c8
Atreya Shankar <shankar.atreya@gmail.com>
2019-11-03 16:46:12 +0100

    *shankar* minor repo renaming and enhanced generator for testing

--- src/obj/RGAN.py
+++ src/obj/RGAN.py
@@ -74,6 +74,15 @@ class RGAN():
         out = Conv2D(64, kernel_size=4, padding="same")(out)
         out = BatchNormalization(momentum=momentum)(out)
         out = Activation("relu")(out)
+        out = Reshape((28,28*64))(out)
+        if len(backend.tensorflow_backend._get_available_gpus()) > 0:
+            out = CuDNNLSTM(28,return_sequences=True,
+                       kernel_constraint=max_norm(3),
+                       recurrent_constraint=max_norm(3),bias_constraint=max_norm(3))(out)
+        else:
+            out = LSTM(28,return_sequences=True,
+                       kernel_constraint=max_norm(3),
+                       recurrent_constraint=max_norm(3),bias_constraint=max_norm(3))(out)
         # block 3
         out = Conv2D(32, kernel_size=3, padding="same")(out)
         out = BatchNormalization(momentum=momentum)(out)
@@ -83,15 +92,6 @@ class RGAN():
         out = BatchNormalization(momentum=momentum)(out)
         out = Activation("relu")(out)
         # reshape
-        out = Reshape((28,28*32))(out)
-        if len(backend.tensorflow_backend._get_available_gpus()) > 0:
-            out = CuDNNLSTM(28,return_sequences=True,
-                       kernel_constraint=max_norm(3),
-                       recurrent_constraint=max_norm(3),bias_constraint=max_norm(3))(out)
-        else:
-            out = LSTM(28,return_sequences=True,
-                       kernel_constraint=max_norm(3),
-                       recurrent_constraint=max_norm(3),bias_constraint=max_norm(3))(out)
         return Model(inputs=in_data,outputs=out)
 
     def getDiscriminator(self,im_dim,droprate,momentum,alpha):
