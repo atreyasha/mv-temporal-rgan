0aaa30c51e2a12d9123a9e297be3cf480e16cb3a
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-01-09 03:29:16 +0100

    Improvement of pre-commit hook

--- src/init.sh
+++ src/init.sh
@@ -3,21 +3,21 @@ set -e
 
 read -rep "create pre-commit hook for updating python dependencies? (y/n): " ans
 if [ $ans == "y" ]; then
-    # move pre-commit hook into local .git folder for activation
-    cp ../hooks/pre-commit.sample ../.git/hooks/pre-commit
+  # move pre-commit hook into local .git folder for activation
+  cp ../hooks/pre-commit ../.git/hooks/pre-commit
 fi
 
 read -rep "download and unzip lfw-faces data? (y/n): " ans
 if [ $ans == "y" ]; then
-    # get lfw-faces data
-    cd ./data
-    wget -O lfwcrop_grey.zip http://conradsanderson.id.au/lfwcrop/lfwcrop_grey.zip
-    unzip -o lfwcrop_grey.zip
-		mv lfwcrop_grey.zip lfwcrop_grey
-    cd ..
+  # get lfw-faces data
+  cd ./data
+  wget -O lfwcrop_grey.zip http://conradsanderson.id.au/lfwcrop/lfwcrop_grey.zip
+  unzip -o lfwcrop_grey.zip
+  mv lfwcrop_grey.zip lfwcrop_grey
+  cd ..
 fi
 
 read -rep "pre-process lfw-faces into numpy binary? (y/n): " ans
 if [ $ans == "y" ]; then
-    python3 pre_process_faces.py
+  python3 pre_process_faces.py
 fi
